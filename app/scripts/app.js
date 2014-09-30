d57673dba531912e9b4341b51c9d93325398a7c4
langz <anderslangseth@hotmail.com>
2014-09-30 18:07:02 +0200

    Scaffolded the project with Yeoman

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name tasksApp
+ * @description
+ * # tasksApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('tasksApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
