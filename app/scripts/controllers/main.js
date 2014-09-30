ce12e7491835e7a1c514ae918a87ea5b8a2e052b
langz <anderslangseth@hotmail.com>
2014-09-30 20:06:54 +0200

    Added some more features

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -14,12 +14,12 @@ angular.module('tasksApp')
 
   $scope.tasks = $firebase(tasksRef);
 
-  $scope.newTask = {name: '', information: '', hours: '', created: '', done: false};
+  $scope.newTask = {name: '', information: '', hours: '', created: '', done: false, notified: 'No'};
 
   $scope.saveTask = function(){
     $scope.newTask.created = new Date();
     $scope.tasks.$add($scope.newTask);
-    $scope.newTask = {name: '', information: '', hours: '', created: '', done: false};
+    $scope.newTask = {name: '', information: '', hours: '', created: '', done: false, notified: 'No'};
 
   };
 
@@ -28,10 +28,11 @@ angular.module('tasksApp')
     var textMessages = $firebase(notificationRef);
     var date = new Date(task.created);
     var dateString = date.toString();
-    var messageText = 'Hi, this is a notification for the Task ' + task.name + ', ' + task.information + '. It was created at ' + dateString  + ' and is estimated to take ' + task.hours + ' hours';
+    var messageText = 'Task ' + task.name + ', ' + task.information + '. Created at ' + dateString  + ' and is estimated to take ' + task.hours + ' hours';
     textMessages.$add({phoneNumber: 4793086048, message: messageText});
 
-
+task.notified ='Yes';
+$scope.tasks.$save(task.$id);
   };
 
 }]);
